167a8a53e92772eca9d8c4ba56ae36e016e80a98
charliecurtin1 <charliecurtin1@gmail.com>
2023-08-25 11:07:14 -0700

    my thing

--- R/theme_chooch.R
+++ R/theme_chooch.R
@@ -7,6 +7,9 @@
 theme_chooch <- function() {
   theme(plot.background = element_rect(fill = "pink"),
         panel.background = element_rect(fill = "magenta"),
-        axis.text = element_text(color = "red"),
-        panel.grid = element_line(color = "salmon"))
+        axis.text = element_text(color = "red", ),
+        panel.grid = element_line(color = "salmon"),
+        legend.background = element_rect(fill = "gray"),
+        title = element_text("ugly ugly ugly graph"),
+        legend.key.size = 10)
 }
